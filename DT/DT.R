164f00f21bd041f71d990d6c4997bfef94c51080
Deepaneesh <deepaneesh98@gmail.com>
2025-07-17 15:40:28 +0530

    DT is addded

---
+++ DT/DT.R
@@ -0,0 +1,20 @@
+library(DT)
+data=iris
+datatable(
+  data,
+  filter = "top",
+  rownames = FALSE,
+  editable = FALSE,
+  extensions = c("Buttons", "Scroller", "Responsive", "KeyTable", "SearchBuilder"),
+  options = list(
+    keys = TRUE,
+    scrollY = 400,
+    scrollX = 200,
+    responsive = FALSE,
+    pageLength = 10,
+    lengthMenu = c(10, 25, 30),
+    dom = 'QBlfrtip',  # ✅ Q = SearchBuilder, B = Buttons
+    searchBuilder = TRUE,
+    buttons = c('copy', 'csv', 'pdf', 'print')
+  )
+)
